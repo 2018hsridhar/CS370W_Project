7c63b603e115a38e4632e0facbaffb51304f13c2
Hari Sridhar <hari2018@cs.utexas.edu>
2017-03-19 18:33:35 -0500

    SGD is not correct. This is not a Rigid scheme, from what I am seeing, AND in some cases, I getr reasonable output, OTHER cases, unreasonable output

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 
 // LibIgl includes
 #include <igl/writeOFF.h>
+#include <igl/viewer/Viewer.h>
+#include <igl/readOFF.h>
 
 using namespace Eigen;  
 using namespace std;
@@ -20,8 +22,49 @@ struct Mesh
 } scan1,scan2,interp,remeshed, result;
 
 // testing one iteration of SGD - from 1 matrices ( init @ I ) to 5 matrices ( @ what SGD produes for thee ). I should desend specifically on the one with LOWEST energy!
+void runPipeline();
+//bool key_down(igl::viewer::Viewer& viewer, unsigned char key, int modifier);
 
 int main(int argc, char *argv[])
+{
+	runPipeline();
+	
+	Eigen::MatrixXd V;
+	Eigen::MatrixXi F;	
+	igl::readOFF(GLOBAL::pipelineOutputFile, V, F);
+	igl::viewer::Viewer viewer;
+	viewer.data.set_mesh(V,F);
+	viewer.launch();
+}
+
+/*
+
+// in a couple of wyas, setting up this visualizer seems like a dumb idea
+// it is low-hanging fruit, but  ... IT IS ALSO NOT THAT CRUCIAL!!
+
+bool key_down( igl::viewer::Viewer& viewer, unsigned char key, int modifier)
+{
+	std::cout << "Key : " << key << (unsigned int) key << std::endl;
+	if ( key == 'r' || key == 'R' ) 
+	{
+		viewer.data.clear();
+	}
+	else if ( key == ' ' )
+	{
+		viewer.data.clear();
+		applyOneTimeStepOfPipeline();
+		viewer.data.set_mesh(V_mcf,F_one);
+		viewer.core.align_camera_center(V_mcf,F_one);
+		std::string output_of_mesh = "meanCurvaureFlowOutput[" + std::to_string(k) + "].stl";
+		std::cout << "WRITING MCF data for iteration [" << std::to_string(k) << " ].\n"; 
+		igl::writeOFF(output_of_mesh, V_mcf, F_one);
+	} 
+	return false;
+}
+*/
+	
+// let me modify the method, to view stepping - BUT, don't PRINT out the output file, just yet!
+void runPipeline()
 {
 	std::cout << "Executing Zero-Overlap Rigid Alignment Pipeline" << std::endl;
 
@@ -42,8 +85,9 @@ int main(int argc, char *argv[])
 	std::vector<Eigen::Matrix4d> transMats;	
 	transMats.push_back(T);
 
-	// FOR NOW, RUNNING PIPELINE FOR A FIXED NUMBER OF ITERATIONS
-	for(int k = 0; k < 10; ++k)
+	// k = 10, 15 ... get reasonable output
+	// k = 20 = GAAH
+	for(int k = 0; k < 20; ++k)
 	{
 		cout << "Running SGD for [" << k << "] th iteration" << endl;;
 		std::vector<double> energies;
@@ -59,7 +103,6 @@ int main(int argc, char *argv[])
 
 			cout << "Generating interpolating surface" << endl;
 			INTERP_SURF::generateOffsetSurface(transScan1,scan1.F,scan2.V,scan2.F, interp.V,interp.F);
-
 			cout << "Remeshing interpolating surface" << endl;
 			double remeshEdgeLen = REMESH::avgEdgeLenInputMeshes(transScan1,scan1.F,scan2.V,scan2.F);
 			REMESH::remeshSurface(interp.V,interp.F,remeshed.V,remeshed.F, remeshEdgeLen);
@@ -87,7 +130,6 @@ int main(int argc, char *argv[])
 	cout << "END RESULT transition matrix is : " << endl;
 	cout << T << endl;
 
-
 	// write data to output file  - create one huge mesh
 	// result.V, result.F
 	Eigen::MatrixXd transScan1;	
@@ -97,7 +139,6 @@ int main(int argc, char *argv[])
 	igl::writeOFF(GLOBAL::pipelineOutputFile, result.V, result.F);
 
 	cout << "Wrote off file for aligned meshes" << endl;
-    return 0;
 }
 
-    
+
